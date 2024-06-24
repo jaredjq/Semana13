3759453f31560972947a37ca570c3df9b6a9b02b
jared <2017230179@untels.edu.pe>
2024-06-24 18:49:13 -0500

    Subiendo semana 13

---
+++ proyecto001/miapp/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+# Create your views here.
+
+layout = """
+    <h1> Proyecto Web (LP3 - 2024) | Flor Cerdán </h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inicio"> Inicio</a>
+        </li>
+        <li>
+            <a href="/saludo"> Mensaje de Saludo</a>
+        </li>
+        <li>
+            <a href="/rango"> Mostrar Números [a,b]</a>
+        </li>
+        <li>
+            <a href="/rango2/10/15"> Mostrar Números [10,15]</a>
+        </li>
+
+    </ul>
+    <hr/>
+"""
+
+
+def index(request):
+    estudiantes = [ 'Isabella Caballero', 
+                    'Alejandro Hermitaño',
+                    'Joan Palomino',
+                    'Pierre Bernaola']
+
+
+    return render(request,'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web Con DJango',
+        'estudiantes': estudiantes
+    })
+
+
+
+
+def saludo(request):
+    return render(request,'saludo.html',{
+        'titulo':'Saludo',
+        'autor_saludo':'Mg. Flor Elizabeth Cerdán León'
+    })
+
+
+
+
+def rango(request):
+    a = 10
+    b = 20
+    rango_numeros = range(a,b+1)
+    return render(request,'rango.html',{
+        'titulo':'Rango',
+        'a':a,
+        'b':b,
+        'rango_numeros':rango_numeros
+    })
+
+def rango2(request,a,b):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
